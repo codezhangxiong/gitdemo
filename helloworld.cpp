bd9470c214f57452a8e15c9e25ce3717d108430e
hx-zhangxiong <hellozhangxiong@163.com>
2022-11-22 11:20:41 +0800

    修改了helloworld.cpp文件

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,9 @@ int main()
 {
     printf("hello world!");
     printf("this is apple");
-    printf("this is banana!");
+    printf("this is banana!\n");
+
+    printf("today is 20221122\n");
+
     return 0;
 }
